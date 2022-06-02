723e5edb5c423707048bab932a2828aaad7af11c
Luciana Santos <luguys@gmail.com>
2022-06-01 23:50:59 -0300

    aula1

---
+++ ui/src/components/CategorySelect/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Select } from 'antd';
+
+const CategorySelect = () => {
+  return (
+    <Select style={{width:"250px"}}>
+      <Select.Option>Categoria</Select.Option>
+    </Select>
+  );
+};
+export default CategorySelect;
---
+++ ui/src/containers/DashboardContainer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CategorySelect from "../../components/CategorySelect";
+import "./index.less";
+
+
+
+function DashboardContainer() {
+ 
+
+  return (
+    <div className="my-dashboard">
+     <CategorySelect />
+    </div>
+  );
+}
+
+export default DashboardContainer;
